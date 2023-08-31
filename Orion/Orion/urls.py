37f39de47eba7e6037c40e42a9af53c0306085e7
Kelvin Cardoso <109598823+belokelvin@users.noreply.github.com>
2023-08-31 02:58:41 -0300

    -- ok

--- Orion/Orion/urls.py
+++ Orion/Orion/urls.py
@@ -4,5 +4,7 @@ from app_Login import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('usuarios/', views.go_cadastrar, name='go_cadastrar'),
+    path('usuarios/', views.go_login, name='go_login'),
     # Adicione esta linha para a página de boas-vindas
 ]
--- Orion/app_Login/views.py
+++ Orion/app_Login/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 
-
 def home(request):
     return render(request, 'usuarios/home.html')
+
+def go_cadastrar(request):
+    return render(request, 'usuarios/Cadastro.html')
+
+def go_login(request):
+    return render(request, 'usuarios/Login.html')
